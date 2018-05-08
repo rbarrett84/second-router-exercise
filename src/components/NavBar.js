b707dd5b883f381dc86fec3b5901faae67bb5d7a
rbarrett84 <rbarrett84@yahoo.com>
2018-05-08 13:42:34 -0400

    links and routes added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,7 +11,18 @@ class NavBar extends Component{
 
   render(){
     return(
-      <h3>Create Links Here</h3>
+      <div>
+      <div className="navbar">
+        <Link to='/'> HOME </Link>
+        <Link to='/greeting'>GREETING</Link>
+        <Link to='/goodbye'>GOODBYE</Link>
+        <Link to='/custom'>CUSTOM GREETING</Link>
+      </div>
+      <div className="content">
+        <h1 className="page-title">Ways to say Hello and Goodbye</h1>
+        {props.children}
+      </div>
+    </div>
     )
   }
 }
